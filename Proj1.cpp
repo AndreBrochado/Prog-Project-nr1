86ee740058b759c362305e7f579da93b81155cfd
André Reis <ansore.b.reisu@gmail.com>
2015-04-17 00:39:58 +0100

    table with finished, color to int conversion with potato code

--- Proj1.cpp
+++ Proj1.cpp
@@ -6,23 +6,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <windows.h>
-
-#define BLACK 0
-#define BLUE 1
-#define GREEN 2
-#define CYAN 3
-#define RED 4
-#define MAGENTA 5
-#define BROWN 6
-#define LIGHTGRAY 7
-#define DARKGRAY 8
-#define LIGHTBLUE 9
-#define LIGHTGREEN 10
-#define LIGHTCYAN 11
-#define LIGHTRED 12
-#define LIGHTMAGENTA 13
-#define YELLOW 14
-#define WHITE 15
+#include <locale>
 
 using namespace std;
 
@@ -39,7 +23,7 @@ struct boat{
 void setcolor(unsigned int color, unsigned int background_color)
 {
 HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-if (background_color == BLACK)
+if (background_color == 0)
 SetConsoleTextAttribute(hCon, color);
 else
 SetConsoleTextAttribute(hCon, color | BACKGROUND_BLUE | BACKGROUND_GREEN |
@@ -53,6 +37,28 @@ HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
 SetConsoleTextAttribute(hcon, color);
 }
 
+//returns the integral value for a string with the name of the color
+int colorValue(string color){
+        if (color == "Preto") return 0;
+        if (color == "Azul") return 1;
+        if (color == "Verde") return 2;
+        if (color == "Ciano") return 3;
+        if (color == "Vermelho") return 4;
+        if (color == "Magenta") return 5;
+        if (color == "Castanho") return 6;
+        if (color == "Cinzento_claro") return 7;
+        if (color == "Cinzento_escuro") return 8;
+        if (color == "Azul_claro") return 9;
+        if (color == "Verde_claro") return 10;
+        if (color == "Ciano_claro") return 11;
+        if (color == "Vermelho_claro") return 12;
+        if (color == "Magenta_claro") return 13;
+        if (color == "Amarelo") return 14;
+        if (color == "Branco") return 15;
+        return -1;
+}
+
+//checks if the position given for the given ship is valid (doesn't cross other boats or goes out of the board)
 bool checkValidPosition(const vector< vector<char> >& table, boat ship){
 	bool valPos=true;
 	if(ship.xPos<'a' || ship.xPos>'a'+table[0].size() || ship.yPos<'A' || ship.yPos>'A'+table.size())
@@ -72,7 +78,17 @@ bool checkValidPosition(const vector< vector<char> >& table, boat ship){
 	return valPos;
 }
 
-void printTable(const vector< vector<char> >& table){
+//Returns the color of the boat with given symbol
+int boatColor(const vector<boat> &boatList, char symbol){
+	for(size_t i=0; i<boatList.size(); i++){
+		if(boatList[i].symbol == symbol){
+			return colorValue(boatList[i].color);
+		}
+	}
+	return -1;
+}
+
+void printTable(const vector< vector<char> >& table, const vector<boat> &boatList){
 	cout << " ";
 	setcolor(15);
 	for(size_t k=0; k<table.size();k++){
@@ -83,15 +99,19 @@ void printTable(const vector< vector<char> >& table){
 	for(size_t i=0; i<table.size(); i++){
 		for(size_t j=0; j<table[0].size(); j++){
 			if(j==0){
-				setcolor(15,0);
+				setcolor(0,15);
 				cout << char ('A'+i);}
+			else if(table[i][j]!='.')
 			setcolor(9,7);
+			else
+				setcolor(boatColor(boatList, table[i][j]),7);
 			cout<< setw(2) << table[i][j];
 		}
 		cout<<endl;
 	}
 }
 
+// void addBoat(vector< vector<char> > &table, boat)
 int main(){
 	string fileName, dimensions, trash; //trash will be used to get full words like the board name
 	char aux; //gets out chars like 'x' and '-'
@@ -122,6 +142,6 @@ int main(){
 		boatList.push_back(tempBoat);
 
 	}while(!configFile.eof());
-	printTable(table);
+	printTable(table, boatList);
 	return 0;
 }
