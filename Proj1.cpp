9c3cf685f103f794e078b79882488d36b45a47ff
Ansore <ansore.b.reisu@gmail.com>
2015-04-17 19:25:19 +0100

    Fixed the checkValidPosition func

--- Proj1.cpp
+++ Proj1.cpp
@@ -13,7 +13,7 @@ using namespace std;
 struct boat{
 	int number;
 	string name;
-	int size;
+	unsigned int size;
 	char symbol;
 	string color;
 	unsigned char xPos, yPos, orientation;
@@ -22,40 +22,40 @@ struct boat{
 // Set text color & background
 void setcolor(unsigned int color, unsigned int background_color)
 {
-HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-if (background_color == 0)
-SetConsoleTextAttribute(hCon, color);
-else
-SetConsoleTextAttribute(hCon, color | BACKGROUND_BLUE | BACKGROUND_GREEN |
-BACKGROUND_RED);
+	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (background_color == 0)
+		SetConsoleTextAttribute(hCon, color);
+	else
+		SetConsoleTextAttribute(hCon, color | BACKGROUND_BLUE | BACKGROUND_GREEN |
+				BACKGROUND_RED);
 }
 
 // Set text color
 void setcolor(unsigned int color)
 {
-HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-SetConsoleTextAttribute(hcon, color);
+	HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hcon, color);
 }
 
 //returns the integral value for a string with the name of the color
 int colorValue(string color){
-        if (color == "Preto") return 0;
-        if (color == "Azul") return 1;
-        if (color == "Verde") return 2;
-        if (color == "Ciano") return 3;
-        if (color == "Vermelho") return 4;
-        if (color == "Magenta") return 5;
-        if (color == "Castanho") return 6;
-        if (color == "Cinzento_claro") return 7;
-        if (color == "Cinzento_escuro") return 8;
-        if (color == "Azul_claro") return 9;
-        if (color == "Verde_claro") return 10;
-        if (color == "Ciano_claro") return 11;
-        if (color == "Vermelho_claro") return 12;
-        if (color == "Magenta_claro") return 13;
-        if (color == "Amarelo") return 14;
-        if (color == "Branco") return 15;
-        return -1;
+	if (color == "Preto") return 0;
+	if (color == "Azul") return 1;
+	if (color == "Verde") return 2;
+	if (color == "Ciano") return 3;
+	if (color == "Vermelho") return 4;
+	if (color == "Magenta") return 5;
+	if (color == "Castanho") return 6;
+	if (color == "Cinzento_claro") return 7;
+	if (color == "Cinzento_escuro") return 8;
+	if (color == "Azul_claro") return 9;
+	if (color == "Verde_claro") return 10;
+	if (color == "Ciano_claro") return 11;
+	if (color == "Vermelho_claro") return 12;
+	if (color == "Magenta_claro") return 13;
+	if (color == "Amarelo") return 14;
+	if (color == "Branco") return 15;
+	return -1;
 }
 
 //checks if the position given for the given ship is valid (doesn't cross other boats or goes out of the board)
@@ -63,18 +63,28 @@ bool checkValidPosition(const vector< vector<char> >& table, boat ship){
 	bool valPos=true;
 	if(ship.xPos<'a' || ship.xPos>'a'+table[0].size() || ship.yPos<'A' || ship.yPos>'A'+table.size())
 		valPos=false;
-	if(! (ship.orientation == 'V' || ship.orientation == 'H'))
+	else if(! (ship.orientation == 'V' || ship.orientation == 'H'))
 		valPos = false;
-	else if(ship.orientation == 'V')
-		for(int i=0; i<ship.size; i++){
-			if(table[ship.xPos-'a'+i][ship.yPos-'A']!='.')
-				valPos = false;
+	else if(ship.orientation == 'V'){
+		if(ship.yPos + ship.size > 'A' + table.size())
+			valPos=false;
+		else{
+			for(size_t i=0; i<ship.size; i++){
+				if(table[ship.xPos-'a'+i][ship.yPos-'A']!='.')
+					valPos = false;
+			}
 		}
-	else if(ship.orientation == 'H')
-		for(int i=0; i<ship.size; i++){
-					if(table[ship.xPos-'a'][ship.yPos-'A'+i]!='.')
-						valPos = false;
-				};
+	}
+	else if(ship.orientation == 'H'){
+		if(ship.yPos + ship.size > 'A' + table.size())
+			valPos=false;
+		else{
+			for(size_t i=0; i<ship.size; i++){
+				if(table[ship.xPos-'a'][ship.yPos-'A'+i]!='.')
+					valPos = false;
+			}
+		}
+	};
 	return valPos;
 }
 
@@ -102,13 +112,20 @@ void printTable(const vector< vector<char> >& table, const vector<boat> &boatLis
 				setcolor(0,15);
 				cout << char ('A'+i);}
 			else if(table[i][j]!='.')
-			setcolor(9,7);
+				setcolor(9,7);
 			else
 				setcolor(boatColor(boatList, table[i][j]),7);
 			cout<< setw(2) << table[i][j];
 		}
 		cout<<endl;
 	}
+}
+
+void addBoat(vector< vector<char> >& table, boat ship){
+	if(ship.orientation == 'V')
+		for(int i = ship.yPos; i<= ship.yPos + ship.size; i++)
+			table[i][ship.xPos]=ship.symbol;
+
 }
 
 // void addBoat(vector< vector<char> > &table, boat)
